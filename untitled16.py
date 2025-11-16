0f5c668074de36eb8ef1cd58b76c4e72683f8a92
Ulugbek-Arziqulov <ulugbekarziqulov94@gmail.com>
2025-11-16 08:06:26 +0500

    yangi mashg'ulotlar
    16-17

--- untitled16.py
+++ untitled16.py
@@ -14,7 +14,7 @@ Created on Sun Nov 16 06:54:45 2025
 ism="ulug'bek"
 yosh=31
 y_shahar="navoiy"
-print(f"Mening ismim {ism.title()}, men {yosh} yoshdaman, hozirda {y_shahar.title()}, shahrida yashayman.")
+print(f"Mening ismim {ism.title()}, men {yosh} yoshdaman, hozirda {y_shahar.title()} shahrida yashayman.")
 
 
 
---
+++ untitled17.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 16 07:03:43 2025
+
+@author: HI-TECH
+"""
+
+# VAZIFA:
+# 1. Ikkita son yarating (masalan: 25 va 5)
+# 2. Qo'shish, ayirish, ko'paytirish, bo'lish natijalarini hisoblang
+# 3. Har birini alohida print qiling (chiroyli formatda)
+
+a=20
+b=10
+print(f"{a}+{b}={a+b}")
+print(f"{a}-{b}={a-b}")
+print(f"{a}*{b}={a*b}")
+print(f"{a}/{b}={a/b}")
+print(f"{a}**{b}={a**b}")
+print(f"{a}//{b}={a//b}")
+
+print(f"{a}+{b}={a+b}, {a}-{b}={a-b}, {a}*{b}={a*b}, {a}/{b}={a/b}, {a}**{b}={a**b}, {a}//{b}={a//b}.")
